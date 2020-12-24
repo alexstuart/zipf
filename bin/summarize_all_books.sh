b62e4b24726243c93047dc335c0feaaa921f3888
Alex Stuart <alex.stuart@jisc.ac.uk>
2020-12-24 15:24:19 +0000

    Add scripts, novels, word counts, and plots

---
+++ bin/summarize_all_books.sh
@@ -0,0 +1,8 @@
+for x in ../data/*.txt
+do
+  echo $x; bash book_summary.sh $x Author;
+done > authors.txt
+for x in ../data/*.txt; do echo $x; bash book_summary.sh $x Release;
+done > releases.txt
+mkdir ../results
+mv authors.txt releases.txt ../results
